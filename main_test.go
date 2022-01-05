634bd0f0731ae13c3be40099294cea09c09d5a36
Serge Zaitsev <serge.zaitsev@grafana.com>
2022-01-05 10:56:10 +0100

    init array

--- main_test.go
+++ main_test.go
@@ -2,7 +2,11 @@ package main
 
 import "testing"
 
-var array = []int{1, 2, 3, 5, 7, 11, 13, 17}
+var array []int
+
+func init() {
+	array = []int{1, 2, 3, 5, 7, 11, 13, 17, 19, 23}
+}
 
 func TestIt(t *testing.T) {
 	t.Log(array)
